8c100b255051b459369f8fd4dc0623b6434606b1
Nikolaus Stoll <mail@nikolausstoll.de>
2026-07-06 14:31:26 +0200

    chore: Update package-lock.json and configure Vite server settings
    - Bumped versions of several dependencies in package-lock.json, including @vitest packages and es-module-lexer.
    - Configured Vite server settings to enable host binding, set port to 5173, and enforce strict port usage for improved development experience.

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,9 @@ export default defineConfig({
   },
   base: "./",
   server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
     proxy: {
       '/api': {
         target: 'http://localhost:8098',
